fe3afa51fed2400ca9df1c18cd38cb29d5386840
Dmitry Petrishin <>
2018-09-20 15:58:12 +0300

    Initial

---
+++ cummulative-sum-query.sql
@@ -0,0 +1,28 @@
+WITH SubTotalTable AS (
+	SELECT 
+		c.[ClientName]
+		,YEAR(c.[Date])  as [Year]
+		,MONTH(c.[Date]) as [Month]
+		,SUM(c.[Amount]) as [SubTotal] 
+	FROM 
+		Clients c
+	WHERE 
+		YEAR(c.[Date]) = '2017' 
+	GROUP BY 
+		c.[ClientName]
+		,YEAR(c.[Date])
+		,MONTH(c.[Date]) 
+) 
+SELECT 
+	[ClientName]
+	,[Month]
+	,SUM([SubTotal]) OVER (
+		PARTITION BY 
+			[ClientName] 
+		ORDER BY [Month] 
+		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as [Cumulative Sum] 
+FROM 
+	SubTotalTable
+ORDER BY 
+	[ClientName] DESC,
+	[Month]
